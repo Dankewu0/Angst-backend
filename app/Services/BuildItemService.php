da508f6ebea9c8f090f541688e1a68d8a79e14da
Dankewu <wherephantom@gmail.com>
2026-03-28 22:09:28 +0300

    chore: minor updates

---
+++ app/Services/BuildItemService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\BuildItem;
+
+class BuildItemService
+{
+}
---
+++ app/Services/BuildService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Build;
+
+class BuildService
+{
+}
